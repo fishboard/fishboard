9478252af673779caa15d2808df7fc9c257e3a95
Hasan Ozgan <hasanozgan@gmail.com>
2015-10-21 18:25:56 +0300

    file structure changed

--- app/common/accounts/accounts_config.js
+++ app/common/accounts/accounts_config.js
@@ -1,9 +1,11 @@
-Accounts.ui.config({
-  passwordSignupFields: 'EMAIL_ONLY'
-  //passwordSignupFields: 'USERNAME_AND_EMAIL'
-  //passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
-  //passwordSignupFields: 'EMAIL_ONLY' // deafult
-});
+if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: 'EMAIL_ONLY'
+    //passwordSignupFields: 'USERNAME_AND_EMAIL'
+    //passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+    //passwordSignupFields: 'EMAIL_ONLY' // deafult
+  });  
+}
 
 Meteor.startup(function () {
     AccountsEntry.config({
